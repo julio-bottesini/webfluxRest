cf32d184212255fee5b697773cf6a4690deee0f2
Julio Bottesini <julio_bottesini@hotmail.com>
2019-05-26 00:02:49 -0300

    Project creation

---
+++ src/main/java/br/com/bottesini/webflux/WebfluxRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.bottesini.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxRestApplication.class, args);
+    }
+
+}
